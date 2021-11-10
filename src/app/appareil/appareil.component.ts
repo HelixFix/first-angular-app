c77e16a6668405c5c6db7560d60b36588d493e4b
HelixFix <jerome.rinner@gmail.com>
2021-11-10 11:38:29 +0100

    Custom property with @input alias

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-appareil',
+  selector   : 'app-appareil',
   templateUrl: './appareil.component.html',
-  styleUrls: ['./appareil.component.scss']
+  styleUrls  : ['./appareil.component.scss']
 })
 export class AppareilComponent implements OnInit {
 
-  appareilName = "Machine à laver";
-  appareilStatus = "Éteint";
+  @Input() appareilName  : string;
+  @Input() appareilStatus: string;
 
   constructor() { }
 
