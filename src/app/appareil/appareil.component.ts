b5b53ebb469d474d18d54b421912fad2d08055a7
HelixFix <jerome.rinner@gmail.com>
2021-11-11 13:13:17 +0100

    :art: Indent codes

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-appareil',
+  selector   : 'app-appareil',
   templateUrl: './appareil.component.html',
-  styleUrls: ['./appareil.component.scss'],
+  styleUrls  : ['./appareil.component.scss'],
 })
 export class AppareilComponent implements OnInit {
-  @Input() appareilName: string;
+  @Input() appareilName  : string;
   @Input() appareilStatus: string;
 
   constructor() {}
