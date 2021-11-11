54aa8970db5115976e39f12c33560bef782ddaa0
HelixFix <jerome.rinner@gmail.com>
2021-11-11 11:38:21 +0100

    Add Attribute Directive

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -1,22 +1,29 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector   : 'app-appareil',
+  selector: 'app-appareil',
   templateUrl: './appareil.component.html',
-  styleUrls  : ['./appareil.component.scss']
+  styleUrls: ['./appareil.component.scss'],
 })
 export class AppareilComponent implements OnInit {
-
-  @Input() appareilName  : string;
+  @Input() appareilName: string;
   @Input() appareilStatus: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   getStatus() {
-    return this.appareilStatus
+    return this.appareilStatus;
   }
 
+  getColor() {
+    if (this.appareilStatus === 'allumé') {
+      return 'green';
+    } else if (this.appareilStatus === 'éteint') {
+      return 'red';
+    } else {
+      return '';
+    }
+  }
 }
