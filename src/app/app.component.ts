266d4782fe2c56eb4a1a1b4eaeb59f8f7e2b62f9
HelixFix <jerome.rinner@gmail.com>
2021-11-16 11:14:22 +0100

    :recycle: Move codes from app-components.rs -> appareil-view.components.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { AppareilService } from './services/appareils.services';
+import { Component } from '@angular/core';
 
 @Component({
   selector   : 'app-root',
   templateUrl: './app.component.html',
   styleUrls  : ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
-  isAuth = false;
+export class AppComponent{
+  constructor() {}
 
-  lastUpdate = new Promise<Date>(
-    (resolve, reject) => {
-      const date = new Date();
-      setTimeout(() => {
-        resolve(date);
-      }, 2000);
-    }
-  );
-
-  appareils: any[];
-
-  constructor(private appareilService: AppareilService) {
-    setTimeout(() => {
-      this.isAuth = true;
-    }, 4000);
-  } 
-  
-
-  ngOnInit() {
-
-      // Le tableau local appareils sera égal au tableau dans le service
-    this.appareils = this.appareilService.appareils;
-  }
-  
-  onAllumer() {
-    this.appareilService.switchOnAll()
-  }
-
-  onEteindre() {
-    this.appareilService.switchOffAll()
-  }
 }
--- src/app/appareil-view/appareil-view.component.ts
+++ src/app/appareil-view/appareil-view.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { AppareilService } from '../services/appareils.services';
 
 @Component({
-  selector: 'app-appareil-view',
+  selector   : 'app-appareil-view',
   templateUrl: './appareil-view.component.html',
-  styleUrls: ['./appareil-view.component.scss']
+  styleUrls  : ['./appareil-view.component.scss'],
 })
 export class AppareilViewComponent implements OnInit {
+  isAuth = false;
 
-  constructor() { }
+  lastUpdate = new Promise<Date>((resolve, reject) => {
+    const date = new Date();
+    setTimeout(() => {
+      resolve(date);
+    }, 2000);
+  });
 
-  ngOnInit(): void {
+  appareils: any[];
+
+  constructor(private appareilService: AppareilService) {
+    setTimeout(() => {
+      this.isAuth = true;
+    }, 4000);
   }
 
+  ngOnInit() {
+    // Le tableau local appareils sera égal au tableau dans le service
+    this.appareils = this.appareilService.appareils;
+  }
+
+  onAllumer() {
+    this.appareilService.switchOnAll();
+  }
+
+  onEteindre() {
+    this.appareilService.switchOffAll();
+  }
 }
