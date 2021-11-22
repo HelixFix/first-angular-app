d29cef93d771bb40bf05643004f4ca090570288b
HelixFix <jerome.rinner@gmail.com>
2021-11-22 14:58:48 +0100

    Create first Observable counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, interval } from 'rxjs';
 
 @Component({
   selector   : 'app-root',
   templateUrl: './app.component.html',
   styleUrls  : ['./app.component.scss'],
 })
-export class AppComponent{
+export class AppComponent implements OnInit {
+  secondes: number;
+
   constructor() {}
 
+  ngOnInit() {
+    const counter = interval(1000);
+    counter.subscribe({
+      next: (value: number) => (this.secondes = value),
+      error: () => console.log('Une erreur a été rencontrée !'),
+      complete: () => console.info('Observable complété !'),
+    });
+  }
 }
