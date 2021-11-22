6bc7931e23070ef1fbe13d01ca9d5e94a1c2314a
HelixFix <jerome.rinner@gmail.com>
2021-11-22 15:04:45 +0100

    Change Obserbable method for Subscription method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, interval } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector   : 'app-root',
   templateUrl: './app.component.html',
   styleUrls  : ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  secondes: number;
+export class AppComponent implements OnInit, OnDestroy {
+  secondes           : number;
+  counterSubscription: Subscription
 
   constructor() {}
 
   ngOnInit() {
     const counter = interval(1000);
-    counter.subscribe({
-      next: (value: number) => (this.secondes = value),
-      error: () => console.log('Une erreur a été rencontrée !'),
-      complete: () => console.info('Observable complété !'),
-    });
+    this.counterSubscription = counter.subscribe(
+      (value: number) => {
+        this.secondes = value;
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    this.counterSubscription.unsubscribe();
   }
 }
